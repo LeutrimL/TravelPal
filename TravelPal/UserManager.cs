3da85c78d00e673be3a424e58cfa959768fdbc10
LeutrimL <146065873+LeutrimL@users.noreply.github.com>
2023-10-30 20:36:14 +0100

    Comments
    I made some Comments to help with the code, me or other people!!

--- TravelPal/UserManager.cs
+++ TravelPal/UserManager.cs
@@ -24,6 +24,7 @@ namespace TravelPal
         {
             if (user !=null && Users.Contains(user))
             {
+                
                 Users.Remove(user);
             }
         } 
